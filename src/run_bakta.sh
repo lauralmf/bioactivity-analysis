2aa869a45cb2025568dee09575acb2f10e6520af
Laura <100786959+lauralmf@users.noreply.github.com>
2025-11-19 16:59:07 +0100

    Update run_bakta.sh

--- src/run_bakta.sh
+++ src/run_bakta.sh
@@ -3,7 +3,7 @@
 # Create folder for bakta results if not present and change directory to the results folder.
 mkdir -p bakta_results && cd bakta_results
 
-# Run script
+# Run bakta on each MAG:
 for file in ../assembly_files/*.fasta; do bakta --db db "$file"; done
 
 # For each iteration, a bakta output folder is made for a single MAG and saved to the bakta_results folder.
