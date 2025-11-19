21abe3d0c56c9cd29acce459e37e3263d45184cb
Laura <100786959+lauralmf@users.noreply.github.com>
2025-11-19 17:04:09 +0100

    Update run_busco.sh

--- src/run_busco.sh
+++ src/run_busco.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd assembly_files
+# Make output folder and change directory to output folder.
+mkdir -p busco_results && cd busco_results
 
-for file in ./*.fasta; do busco -i "$file" -m genome --lineage_dataset bacteria_odb12 -c 8; done
+for file in ../assembly_files/*.fasta; do busco -i "$file" -m genome --lineage_dataset bacteria_odb12 -c 8; done
+
+# You might need to do 
+# for file in ./*.fasta; do busco -i "$file" -m genome --lineage_dataset bacteria_odb12 -c 8; done
+# from in the assembly_files folder and then copy the results to a busco_results folder from the assembly_files folder.
