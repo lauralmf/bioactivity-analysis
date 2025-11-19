c9420e40d9deee6a35c5334b8935c0cf41a258f1
Laura <100786959+lauralmf@users.noreply.github.com>
2025-11-19 16:58:43 +0100

    Update run_antismash.sh

--- src/run_antismash.sh
+++ src/run_antismash.sh
@@ -3,6 +3,7 @@
 # Make folder where the antiSMASH results should be saved and change directory to the results folder.
 mkdir -p antismash_results && cd antismash_results
 
+# Run antiSMASH on each MAG:
 for file in ../assembly_files/*.fasta; do
     base=$(basename "$file" .fasta)  # Extract filename without extension
     antismash "$file" --output-dir "../antismash_results/$base" \
